7ca3bb76a6740a3169eb68aeb9b1758f1772ca34
221029cs06 <113498631+221029cs06@users.noreply.github.com>
2023-02-16 21:32:55 +0300

    Create problem_5.cpp

---
+++ problem_5.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+bool one = true; 
+bool zero = false;
+cout<<zero<<"="<<"false"<<endl;
+cout<<one<<"="<<"true"<<endl;
+ 
+ 
+  
+  return 0;
+}
