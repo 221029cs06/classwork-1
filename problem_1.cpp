660440f409ad5f832ca85a9825200086a54fadca
221029cs06 <113498631+221029cs06@users.noreply.github.com>
2023-02-16 21:22:03 +0300

    Create problem_1.cpp

---
+++ problem_1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int int_num = 15;
+  float float_num = 4.3;
+  double double_num = 76.3982;
+  bool bool_type = false;
+  char char_name = '$';
+  cout<<int_num<<endl;
+  cout<<float_num<<endl;
+  cout<<double_num<<endl;
+  cout<<bool_type<<endl;
+  cout<<char_name;
+  
+  
+  return 0;
+}
